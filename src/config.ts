dc1bb003af60ee979314d13fc36782382a651074
Dustin Rouillard <code@dstn.to>
2021-08-25 18:46:03 -0600

    Update regex

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@ config();
 export const DiscordToken = process.env.BOT_TOKEN as string;
 export const WorkerHost = process.env.WORKER_HOST || 'https://vm.dstn.to';
 
-export const TiktokRegex = /http[s]:\/\/(?:(vm\.)|(www\.))?tiktok\.com\/(?:(.*)|(.*)\/video\/(.*))/i;
+export const TiktokRegex = /http[s]:\/\/(?:vm\.|www\.)?tiktok\.com\/(?:@[a-zA-Z0-9_.]{1,}\/video\/([0-9]{1,})|([a-zA-Z0-9_.]{1,}))/i;
