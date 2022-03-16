2583aae40680a0adb3ae60e8536d75198ade3853
Dustin Rouillard <code@dstn.to>
2022-03-16 15:13:42 -0600

    Add deletion of original message if only a link

--- src/config.ts
+++ src/config.ts
@@ -4,6 +4,7 @@ config();
 export const DiscordToken = process.env.BOT_TOKEN as string;
 export const WorkerHost = process.env.WORKER_HOST || 'https://vm.dstn.to';
 
-export const TiktokRegex = /http[s]:\/\/(?:vm\.|www\.)?tiktok\.com\/(?:@[a-zA-Z0-9_.]{1,}\/video\/([0-9]{1,})|([a-zA-Z0-9_.]{1,}))/i;
+export const URLRegex =
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 
 export const GuildBlacklist = process.env.GUILD_BLACKLIST?.split(',') || [];
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Discord from 'discord.js';
 import { SetConfig, Log } from '@dustinrouillard/fastify-utilities/modules/logger';
 SetConfig({ disableTimestamp: true, logColor: 'reset', debugColor: 'yellow' });
 
-import { DiscordToken, GuildBlacklist, TiktokRegex, WorkerHost } from './config';
+import { DiscordToken, GuildBlacklist, URLRegex, WorkerHost } from './config';
 
 const client = new Discord.Client();
 
@@ -13,9 +13,16 @@ client.on('message', (msg) => {
   if (msg.author.id == client.user?.id) return;
   if (msg.guild && GuildBlacklist.includes(msg.guild.id)) return;
 
-  const matched = TiktokRegex.exec(msg.content);
+  const matched = URLRegex.exec(msg.content);
+
   if (matched && matched[0]) {
-    const url = new URL(matched[0].split(' ')[0].trim());
+    const url = new URL(matched[0]);
+    if (!['vm.tiktok.com', 'tiktok.com'].includes(url.host)) return;
+
+    if (url.host == 'tiktok.com' && !url.pathname.includes('@') && !url.pathname.includes('/video/')) return;
+
+    if (msg.content == matched[0] && msg.deletable) msg.delete();
+
     msg.channel.send(`TikTok shared by ${msg.author.tag}\n${WorkerHost}${url.pathname}`);
   }
 });
